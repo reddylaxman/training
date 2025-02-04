8d289173f085635d4a51a2c273ecf9e5a728ea15
reddylaxman <asl.my.dream@gmail.com>
2025-02-04 12:59:56 +0530

    global css

--- myreact/src/App.js
+++ myreact/src/App.js
@@ -1,9 +1,26 @@
-function App() {
+//rac,rafc,rcc
+import React from "react";
+import { Navbar } from "./components/Navbar";
+import { Main } from "./components/Main";
+import { Sidebar1 } from "./components/Sidebar1";
+import { Sidebar2 } from "./components/Sidebar2";
+import { Footer } from "./components/Footer";
+import CBCPropEx from "./props/CBCPropEx";
+
+export const App = () => {
   return (
-    <div className="App">
-      <h1 style={{ color: "red" }}>App</h1>
+    // <div className="app">
+    //   <Navbar />
+    //   <Main />
+    //   <div className="sidebar">
+    //     <Sidebar1 />
+    //     <Sidebar2 />
+    //   </div>
+    //   <Footer />
+    // </div>
+    <div>
+      <CBCPropEx username="kalki" />
     </div>
   );
-}
-
+};
 export default App;
