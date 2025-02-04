1848fb744774f59e5e82c9f17a93fb25a2686113
reddylaxman <asl.my.dream@gmail.com>
2025-02-04 19:15:43 +0530

    training

--- myreact/src/components/Sidebar1.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Sidebar1 = () => {
-  return <div className="sidebar1">Sidebar1</div>;
-};
--- myreact/src/components/Sidebar2.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Sidebar2 = () => {
-  return <div className="sidebar2">Sidebar2</div>;
-};
